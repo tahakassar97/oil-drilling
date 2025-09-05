09f2fd730389cab0cc9c284a1e2166bcf5860781
tahakassar97 <tahakassar54@gmail.com>
2025-09-05 23:07:51 +0400

    fix script2

--- apps/oil-drilling/next.config.js
+++ apps/oil-drilling/next.config.js
@@ -1,15 +1,5 @@
-//@ts-check
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { composePlugins, withNx } = require('@nx/next');
-
-/**
- * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
- **/
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Use this to set Nx-specific options
-  // See: https://nx.dev/recipes/next/next-config-setup
-  nx: {},
   redirects: async () => {
     return [
       {
@@ -19,11 +9,10 @@ const nextConfig = {
       },
     ];
   },
+  // Ensure proper module resolution
+  experimental: {
+    esmExternals: 'loose',
+  },
 };
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-];
-
-module.exports = composePlugins(...plugins)(nextConfig);
+module.exports = nextConfig;
